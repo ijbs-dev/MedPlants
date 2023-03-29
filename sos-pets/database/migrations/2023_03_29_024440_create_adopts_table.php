c4656fdf4c3ec90c6f16b50516f9e58c10fc2efc
Tuanno <tuannodanyllo26@gmail.com>
2023-03-29 20:05:49 -0300

    criação de sistema de autenticação de cadastro de usuario

--- sos-pets/database/migrations/2023_03_29_024440_create_adopts_table.php
+++ sos-pets/database/migrations/2023_03_29_024440_create_adopts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('adopts', function (Blueprint $table) {
             $table->id();
             $table->date('adoption_date');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');;
-            $table->foreignId('pet_id')->constrained()->onDelete('cascade');;
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('pet_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
