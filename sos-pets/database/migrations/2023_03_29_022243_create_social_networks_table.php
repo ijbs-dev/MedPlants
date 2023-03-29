27a93fbced38cf58df0ecad71ca5bd80caed32a1
Ivan Teotonio <ivanteotonio256@gmail.com>
2023-03-28 23:55:43 -0300

    Criado as tabelas do banco

--- sos-pets/database/migrations/2023_03_29_022243_create_social_networks_table.php
+++ sos-pets/database/migrations/2023_03_29_022243_create_social_networks_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('social_networks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');;
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('telefone',45);
-            $table->boolean('tipo_usuario')->default(false);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('url');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('social_networks');
     }
 };
