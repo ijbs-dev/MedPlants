8b4ac0f5575d2cfd1ec1092eb51299215ace2488
Ivan Teotonio <ivanteotonio256@gmail.com>
2023-05-12 20:33:44 -0300

    resolvendo erros 1.0

--- sos-pets/resources/views/pets/index.blade.php
+++ sos-pets/resources/views/pets/index.blade.php
@@ -6,7 +6,9 @@
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
 @foreach ($pets as $pet)
+
     <img class="w-full" src="{{ url("storage/{$pet->fotos}") }}" alt="{{$pet->nome}}">
+
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">{{ $pet->nome }}</div>
       <p class="text-gray-700 text-base">
