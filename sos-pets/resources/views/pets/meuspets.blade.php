4a1e6e66f010fc620026709c1c4cc738dd1f450f
ivan-teotonio <ivanteotonio256@mail.com>
2023-06-26 18:40:10 -0300

    Tabela meus pets em desenvolvimento

--- sos-pets/resources/views/pets/meuspets.blade.php
+++ sos-pets/resources/views/pets/meuspets.blade.php
@@ -1,13 +1,128 @@
-@extends('principal')
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-@section('titulo', 'Meus Pets')
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
 
-@section('conteudo')
+
+     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
+
+
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
+     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+
+
+     <title>@yield('titulo')</title>
+</head>
+<!--<body class="bg-color-primary text-color-white tracking-wider"> -->
+<body id="page-top" style="background-color:#212529;">
+
+<style>
+
+  #btn-entrar{
+    background-color: #ffc800;
+    color: #000000;
+    border-radius: 15px;
+  }
+
+  #btn-entrar:hover{
+    background-color: #FFA500;
+  }
+
+  #btn-cadastrar{
+    color: #ffffff;
+    font-family: sans-serif;
+    font-size: 15.2px;
+  }
+
+  #btn-cadastrar:hover{
+    color: #ffc800;
+  }
+
+  .sair{
+    color:#ffffff;
+    text-decoration: none;
+    font-family: sans-serif;
+    font-size: 15.2px;
+    padding:8px 24px;
+    margin-top: 50px;
+  }
+
+   .user{
+    color:#696969;
+    text-decoration: none;
+    font-family: sans-serif;
+    font-size: 15.2px;
+    padding:8px 40px;
+    background-color: #ffc800;
+    border-radius: 10px
+  }
+
+  .user:hover{
+    background-color: #ffc800;
+  }
+
+</style>
+
+
+
+ <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="{{route('pets.index')}}">SosPets</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                      @auth
+                        <li class="nav-item"><a class="nav-link" href="{{ route('pets.create') }}">Cadastrar Pets</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('pets.meusPets') }}">Meus Pets</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('pets.agendamentos') }}">Agendamentos</a></li>
+                      @endauth
+                        <li class="nav-item"><a class="nav-link" >Sobre</a></li>
+                        <li class="nav-item"><a href="{{ route('pets.contatos') }}" class="nav-link" >Contato</a></li>
+
+                      @auth
+                      <x-dropdown-link :href="route('profile.edit')" class="user">
+                          {{ __(Auth::user()->name) }}
+                      </x-dropdown-link>
+                      <form method="POST" action="{{ route('logout') }}">
+                      @csrf
+                      <x-dropdown-link class="sair" :href="route('logout')"
+                          onclick="event.preventDefault();
+                          this.closest('form').submit();">
+                          {{ __('Sair') }}
+                      </x-dropdown-link>
+                      </form>
+                      @else
+                        <a href="{{ route('register') }}" class="nav-link" id="btn-cadastrar">Cadastrar</a>
+                        <a href="{{ route('login') }}" id="btn-entrar" class="nav-link">Entrar</a>
+                        @endauth
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
 
 <!--
+
 
 <div class="container mx-auto px-4 sm:px-8">
     <div class="py-8">
@@ -177,7 +292,73 @@
     </div>
 </div>
 
+
 -->
 
 
-@endsection
+
+
+
+<section class="ftco-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center mb-4">
+                <h2 class="heading-section" style="color:#fff;">Meus Pets</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-wrap">
+                    <table class="table">
+                        <thead class="thead-primary">
+                            <tr>
+                                <th>&nbsp;</th>
+                                <th style="font-size: 18px;">Product</th>
+                                <th style="font-size: 18px;">Price</th>
+                                <th style="font-size: 18px;">Price</th>
+                                <th style="font-size: 18px;">Price</th>
+                                <th style="font-size: 18px;">Price</th>
+                                <th style="font-size: 18px;">Price</th>
+                                <th>Ações</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                    <tbody>
+                        <tr class="alert" role="alert">
+                            <td>
+                                <div class="img" style="background-image: url(images/product-1.png);"></div>
+                            </td>
+                            <td>
+                                <div class="email">
+                                    <span>Sneakers Shoes 2020 For Men </span>
+                                    <span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
+                                </div>
+                            </td>
+                            <td>$44.99</td>
+                            <td>$44.99</td>
+                            <td>$44.99</td>
+                            <td>$44.99</td>
+                            <td>$44.99</td>
+                            </td>
+                            <td>$89.98</td>
+                            <td>
+                                <button type="button" class="btn btn-primary btn-sm">Editar</button>
+                                 <button type="button" class="btn btn-danger btn-sm">Excluir</button>
+                            </button>
+                            </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script defer="" src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw==" data-cf-beacon="{&quot;rayId&quot;:&quot;7dd8411a7fb916e9&quot;,&quot;token&quot;:&quot;cd0b4b3a733644fc843ef0b185f98241&quot;,&quot;version&quot;:&quot;2023.4.0&quot;,&quot;si&quot;:100}" crossorigin="anonymous"></script>
+
+</body>
+ 
+
+
+
